a388cc5eb62a37de17cb6195c4138b867cbf01bc
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-05-07 20:08:14 -0300

    Create numeros.py

---
+++ numeros.py
@@ -0,0 +1,5 @@
+numero = int(input("Me informe um número: ")
+sucessor = numero++
+antecessor = numero--
+
+print("Esse é o sucessor do número informado: ", sucessor, "\n", "E esse é o antecessor do número informado: ", antecessor)
