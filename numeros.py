0e8bbd1a721cfc6a835fab6a22cf0f64b9de31e9
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-05-07 20:10:48 -0300

    Update numeros.py

--- numeros.py
+++ numeros.py
@@ -1,5 +1,5 @@
-numero = int(input("Me informe um número: ")
-sucessor = numero++
-antecessor = numero--
+numero = int(input("Me informe um número: "))
+sucessor = numero + 1
+antecessor = numero - 1
 
 print("Esse é o sucessor do número informado: ", sucessor, "\n", "E esse é o antecessor do número informado: ", antecessor)
