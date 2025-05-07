b5e5190e60837957fb11413629386ebae3e236dd
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-05-07 20:03:32 -0300

    Update metros.py

--- metros.py
+++ metros.py
@@ -1,4 +1,4 @@
 metros = int(input("Me informe um valor em metros: "))
-cent = metros / 100
+cent = metros * 100
 
 print("A quantidade em metros informada é igual a esta quantidade de centímetros: ", cent)
